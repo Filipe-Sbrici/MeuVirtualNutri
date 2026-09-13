5100f512e207ca9bc89689f2cb30368412e4f1fb
GabrielFerrareis <gabrielferrareis7@gmail.com>
2026-09-13 19:55:08 -0300

    Teste B1 (cons)

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ const { notFoundHandler, errorHandler } = require('./middlewares/errorHandler');
 const app = express();
 
 // CORS: aceita lista separada por virgula ou "*".
-const origem =
+cons origem =
   config.corsOrigin === '*'
     ? '*'
     : config.corsOrigin.split(',').map((item) => item.trim());
